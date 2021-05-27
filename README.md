a1a7a6f565ce901038b1a52de9b64b6f6db6ef6a
Dao Minh <daoleminh0008@gmail.com>
2021-05-27 18:30:46 +0700

    initialize commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Image Optimization and Compression
+
+## How to use
+
+- Clone the project and `npm i`
+- Put original images in _src_ directory.
+- `npm run start` to optimize and compress images
+- The result will be output in _optimized_ directory
---
+++ index.js
@@ -0,0 +1,58 @@
+const imagemin = require("imagemin");
+const imageminMozjpeg = require("imagemin-mozjpeg");
+const imageminJPEGRecompress = require("imagemin-jpeg-recompress");
+const imageminPngquant = require("imagemin-pngquant");
+const imageminZopfli = require("imagemin-zopfli");
+const imageminSvgo = require("imagemin-svgo");
+const { extendDefaultPlugins } = require("svgo");
+const c = require("ansi-colors");
+
+console.log("-----------------------");
+console.log(c.bgBlue.white("Optimizing in process... Please wait 😃"));
+console.log("-----------------------");
+(async () => {
+  await imagemin(["src/**/*.{jpg,jpeg,png,gif,tiff,svg}"], {
+    destination: "optimized",
+    plugins: [
+      //--------------
+      // For PNG
+      //--------------
+      imageminPngquant({
+        speed: 4,
+        quality: [0.9, 1],
+      }),
+      //--------------
+      // For PNG also, this works together with above method like a layer step
+      //--------------
+      imageminZopfli({ more: true }),
+
+      //--------------
+      // For JPG + JPEG
+      //--------------
+      // imageminMozjpeg({ quality: 70 }),
+      /**
+       *
+       * * Sometimes imageminMozjpeg() make your image become larger because it has a bug
+       * * In that case, comment it out and uncomment imageminJPEGRecompress() below and use this instead.
+       */
+      imageminJPEGRecompress({ target: 0.7 }),
+
+      //--------------
+      // For SVG
+      //--------------
+      imageminSvgo({
+        plugins: extendDefaultPlugins([
+          { name: "removeViewBox", active: false },
+          { name: "removeUnusedNS", active: false },
+          { name: "removeUselessStrokeAndFill", active: false },
+          { name: "cleanupIDs", active: false },
+          { name: "removeComments", active: true },
+          { name: "removeEmptyAttrs", active: true },
+          { name: "removeEmptyText", active: true },
+          { name: "collapseGroups", active: true },
+        ]),
+      }),
+    ],
+  });
+  console.log(c.bgGreen.white("🎉 Done!!! ✅ Your Images Is Optimized. 😎"));
+})();
